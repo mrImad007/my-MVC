a7be7119c374fd87dd531226a9a4c169e18f9fce
imad eddine ZAOUI <imads@imads-MacBook-Pro.local>
2023-02-13 11:09:09 +0100

    MVC insightful architecture

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BURL', "http://mvs_structure/");
+
+define('ROOT', "http://mvs_structure/public");
+
+// define("HOST","localhost");
+// define("USER","root");
+// define("PASS","");
+// define("DB","travigo");
